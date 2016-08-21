ea0123513cbe810fb06810adce2d694f33267b3c
Brad McManus <brad.mcmanus@brigade.com>
2016-08-21 00:39:59 -0700

    Add basic map to Storyboard

--- SFBC/ViewController.swift
+++ SFBC/ViewController.swift
@@ -1,17 +1,29 @@
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+
+    let initialLocation = CLLocation(latitude: 37.769630, longitude: -122.451434)
+    let regionRadius: CLLocationDistance = 1000
+
+    @IBOutlet weak var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
-        
+        centerMapOnLocation(location: initialLocation)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    private func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
     }
 
 
